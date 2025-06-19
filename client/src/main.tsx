aeca7cab2f6f745bf1e65ed3367c5a196813c54e
vileproj <vileprojetos@gmail.com>
2025-06-18 22:05:17 -0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,25 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
-import './index.css';
-import { Toaster } from './components/ui/toaster.tsx';
-import { ThemeProvider } from './components/theme-provider.tsx';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from './lib/queryClient.ts';
-import { ErrorBoundary } from './components/ErrorBoundary.tsx';
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ErrorBoundary>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-          <Toaster />
-        </QueryClientProvider>
-      </ErrorBoundary>
-    </ThemeProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
